e315d666aa44196c5c0f374cabc80d799cfb894f
Snarkturne <snarkturne@gmail.com>
2013-07-30 10:24:40 +0200

    Version 1.0 (posted on Pypi)

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='CAS_bottle',
+    version='1.0',
+    packages=['',],
+    url = 'http://github.com/snarkturne/cas_bottle/',
+    description = '',
+    author = 'SnarkTurne',
+    author_email = 'snarkturne@gmail.com',
+    requires = [
+        'bottle (>=0.9)',
+        'beaker'
+    ],
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
